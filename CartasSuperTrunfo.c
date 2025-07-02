5b5879d22c26e094ead8cc2f08261c94d8475149
Mateus Reis <136511001+MateusReisz@users.noreply.github.com>
2025-07-02 20:40:09 +0000

    Criado o desafio Nivel Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,66 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+struct Carta{
+    char Estado[3];
+    char Codigo[5];
+    char Cidade[50];
+    int Populacao;
+    float Area;
+    float Pib;
+    int Turistico;
+};
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+void preecherCarta(struct Carta *carta){
+    printf("Informe o Estado: ");
+    scanf("%2s", carta->Estado);
+
+    printf("Informe o Codigo: ");
+    scanf("%4s", carta->Codigo);
+
+    printf("Informe a Cidade: ");
+    scanf(" %49[^\n]", carta->Cidade);
+
+    printf("Informe a Populacao: ");
+    scanf("%d", &carta->Populacao);
+
+    printf("Informe a Area: ");
+    scanf("%f", &carta->Area);
+
+    printf("Informe o Pib: ");
+    scanf("%f", &carta->Pib);
+
+    printf("Informe o Ponto Turistico: ");
+    scanf("%d", &carta->Turistico);
+
+    printf(" \n");
+}
+
+void mostrarCarta(struct Carta carta, int numero){
+    printf(" \n");
+    printf("Carta %d:\n", numero);
+    printf("Estado:%s\n", carta.Estado);
+    printf("Codigo:%s\n", carta.Codigo);
+    printf("Cidade:%s\n", carta.Cidade);
+    printf("Populacao:%d\n", carta.Populacao);
+    printf("Area:%.2f\n", carta.Area);
+    printf("PIB:%.2f\n", carta.Pib);
+    printf("Pontos Turisticos:%d\n", carta.Turistico);
+    printf(" \n");
+}
+
+int main(){
+    struct Carta carta1, carta2;
+
+    printf("Cadastro da Carta 1:\n");
+    preecherCarta(&carta1);
+
+    printf("Cadastro da Carta 1:\n");
+    preecherCarta(&carta2);
+
+    printf("=== Dados das Cartas Cadastradas ===\n\n");
+    mostrarCarta(carta1, 1);
+    mostrarCarta(carta2, 2);
 
     return 0;
 }
